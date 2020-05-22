c697500d8bdcf3341234422972d1c89f0abff6bb
Bradley Setzler <bradley.setzler@gmail.com>
2020-05-22 10:24:14 -0500

    add county, CZ, and state distances

--- EconData/R/getMisc.R
+++ EconData/R/getMisc.R
@@ -34,3 +34,81 @@ getStateFips <- function(output_path = "~/github/EconData/DataRepo/Miscellaneous
   
 }
 
+#' CZ distance
+#' @params miles
+#' @params output_path (character)
+#' @export
+distanceCZ <- function(miles=100, output_path = "~/github/EconData/DataRepo/Miscellaneous/"){
+  
+  county_dist <- setDT(read.csv(file=sprintf("%scounty_fips_distance_%smiles.csv", output_path, miles)))
+  county_dist[, county1 := as.character(county1)]
+  county_dist[, county2 := as.character(county2)]
+  county_dist[, len_county1 := nchar(county1)]
+  county_dist[len_county1==4, county1 := paste0("0",county1)]
+  county_dist[, len_county2 := nchar(county2)]
+  county_dist[len_county2==4, county2 := paste0("0",county2)]
+  county_dist[, state_fips1 := as.integer(substr(county1,1,2))]
+  county_dist[, county_fips1 := as.integer(substr(county1,3,5))]
+  county_dist[, state_fips2 := as.integer(substr(county2,1,2))]
+  county_dist[, county_fips2 := as.integer(substr(county2,3,5))]
+  county_dist <- county_dist[,list(state_fips1,county_fips1,state_fips2,county_fips2,distance = mi_to_county)][state_fips1 != "72" & state_fips2 != "72"]
+  
+  county_to_cz <- setDT(read.csv(file=sprintf("%scz_crosswalk_2000.csv",output_path)))
+  setnames(county_to_cz, c("county_fips1","cz1","state_fips1"))
+  county_dist <- merge(county_dist, county_to_cz, by=c("state_fips1","county_fips1"))
+  setnames(county_to_cz, c("county_fips2","cz2","state_fips2"))
+  county_dist <- merge(county_dist, county_to_cz, by=c("state_fips2","county_fips2"))
+  county_dist <- county_dist[cz1 != cz2][, list(distance = min(distance)), list(cz1,cz2)][order(cz1,cz2)]
+  
+  county_dist <- unique(rbind(county_dist, county_dist[,list(cz1=cz2, cz2=cz1, distance)]))
+  
+  write.csv(county_dist,file=sprintf("%sdistances/CZ_distance_%smiles.csv", output_path, miles),row.names=F)
+  
+}
+
+#' State distance
+#' @params miles
+#' @params output_path (character)
+#' @export
+distanceState <- function(miles=100, output_path = "~/github/EconData/DataRepo/Miscellaneous/"){
+  
+  county_dist <- setDT(read.csv(file=sprintf("%scounty_fips_distance_%smiles.csv", output_path, miles)))
+  county_dist[, county1 := as.character(county1)]
+  county_dist[, county2 := as.character(county2)]
+  county_dist[, len_county1 := nchar(county1)]
+  county_dist[len_county1==4, county1 := paste0("0",county1)]
+  county_dist[, len_county2 := nchar(county2)]
+  county_dist[len_county2==4, county2 := paste0("0",county2)]
+  county_dist[, state_fips1 := as.integer(substr(county1,1,2))]
+  county_dist[, county_fips1 := as.integer(substr(county1,3,5))]
+  county_dist[, state_fips2 := as.integer(substr(county2,1,2))]
+  county_dist[, county_fips2 := as.integer(substr(county2,3,5))]
+  county_dist <- county_dist[,list(state_fips1,state_fips2,distance = mi_to_county)][state_fips1 != "72" & state_fips2 != "72"]
+  county_dist <- county_dist[state_fips1 != state_fips2][, list(distance = min(distance)), list(state_fips1, state_fips2)][order(state_fips1, state_fips2)]
+  county_dist <- unique(rbind(county_dist, county_dist[,list(state_fips1=state_fips2, state_fips2=state_fips1, distance)]))
+  write.csv(county_dist,file=sprintf("%sdistances/state_distance_%smiles.csv", output_path, miles),row.names=F)
+  
+}
+
+
+#' Download county distances
+#' @params miles
+#' @params output_path (character)
+#' @export
+prepareDistances <- function(miles=100, output_path = "~/github/EconData/DataRepo/Miscellaneous/"){
+  
+  path <- tempdir()
+  destfile <- sprintf("%s/county_dist.dta", path)
+  county_dist_file <- sprintf("https://data.nber.org/distance/2000/sf1/county/sf12000countydistance%smiles.dta.zip",miles)
+  download.file(county_dist_file,destfile)
+  unzip(zipfile = destfile, exdir = path)
+  county_dist <- setDT(readstata13::read.dta13(file=sprintf("%s/sf12000countydistance%smiles.dta", path, miles)))
+  write.csv(county_dist,file=sprintf("%sdistances/county_fips_distance_%smiles.csv", output_path, miles),row.names=F)
+  file.remove(destfile)
+  
+  distanceCZ(miles=miles, output_path=output_path)
+  distanceState(miles=miles, output_path=output_path)
+  
+}
+
+
